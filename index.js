4bf3548016d0c62cc05b6c9014ddd4cc4d00e13f
Josh <jkiv189@gmail.com>
2020-08-09 21:31:50 -0400

    JoshBot

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+let movieTime = '8pm'
+
+client.once('ready', () => {
+    console.log('JoshBot is online!');
+});
+
+client.on('message', message => {
+    
+    const lowerMessage = message.content.toLowerCase();
+    
+    if (message.author.bot) {
+        return;
+    }
+    
+    if (lowerMessage.startsWith('movie time: ')) {
+        movieTime = message.content.slice(12);
+        return;
+    }
+
+    if (
+      lowerMessage.includes("movie") &&
+      lowerMessage.includes("?") &&
+      (lowerMessage.includes("time") || lowerMessage.includes("when"))
+    ) {
+      message.channel.send(`The time of the movie is ${movieTime}.`);
+    }
+
+});
+
+client.login('NzQwMzkwMzE5MzA4Mjc1ODU0.XyoUNw.jexxueKdMlKKu76eLsGTUIv81bc');
